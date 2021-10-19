ac1353c953ba4cb8f8c3652a1bef49053922a0f0
yunoki <yunoki.hiroshi@gmail.com>
2021-10-19 15:49:58 +0900

    mod

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator } from "aws-amplify-react";
+import { PhotoPicker } from 'aws-amplify-react';
+import { Storage, Auth } from 'aws-amplify';
 
 function App() {
   return (
     <div className="App">
+      <button onClick={() => Auth.signOut()}>サインアウト</button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
